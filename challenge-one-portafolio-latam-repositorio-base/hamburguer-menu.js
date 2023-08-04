d56f5271b1b42a370bdf882f30a36def07723739
Sanrivalin <santiagorivasmolina@gmail.com>
2023-08-04 00:24:09 -0500

    first commit

---
+++ challenge-one-portafolio-latam-repositorio-base/hamburguer-menu.js
@@ -0,0 +1,13 @@
+// // Función para abrir o cerrar el menú
+// function toggleMenu() {
+//     const menu = document.querySelector('.menu');
+//     const menuIcon = document.querySelector('.menu-icon');
+  
+//     if (menu.style.left === '-250px') {
+//       menu.style.left = '0';
+//       menuIcon.textContent = '✕'; // Cambiar el contenido del botón a una 'X'
+//     } else {
+//       menu.style.left = '-250px';
+//       menuIcon.textContent = '☰'; // Cambiar el contenido del botón a las barras horizontales
+//     }
+//   }
